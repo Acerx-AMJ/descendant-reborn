708187a0510950a23c9ec9e3cc2a1589d383d3b7
Acerx-AMJ <ajm.acerx@gmail.com>
2026-05-03 16:38:33 +0300

    Added tile loading.

--- include/data.hpp
+++ include/data.hpp
@@ -15,11 +15,25 @@ struct Level {
    std::vector<std::vector<size_t>> map;
 };
 
+struct Tile {
+   enum class TileType: char {root, ghost, none};
+   enum class Type: char {solid, coin};
+
+   TileType tileType = TileType::none;
+   Type type = Type::solid;
+   size_t ID = 0;
+   size_t tsize = 0;
+   size_t width = 0;
+   size_t height = 0;
+   Texture2D *texture = nullptr;
+};
+
 void loadData();
 void loadPlayerIcons();
 void loadPlayerColors();
 void loadChapters();
 void loadLevels();
+void loadTiles();
 
 size_t getPlayerIconCount();
 std::string &getPlayerIcon(size_t ID);
@@ -36,6 +50,10 @@ std::vector<std::string> &getChapterContainer();
 size_t getLevelCount();
 Level &getLevel(size_t ID);
 std::vector<Level> &getLevelContainer();
+
+size_t getTileCount();
+Tile &getTile(size_t ID);
+std::vector<Tile> &getTileContainer();
 
 // player data module
 
--- source/data.cpp
+++ source/data.cpp
@@ -1,20 +1,32 @@
 #include "data.hpp"
+#include "asset.hpp"
 #include "file.hpp"
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
 
 static std::vector<std::string> playerIconContainer;
 static std::vector<Vector3> playerColorContainer;
 static std::vector<std::string> chapterContainer;
 static std::vector<Level> levelContainer;
+static std::vector<Tile> tileContainer;
+
+static std::unordered_map<std::string_view, size_t> tileNameMap;
 
 // game data module
 
 void loadData() {
+   printf("Loading player icons from 'data/icons.txt'...\n");
    loadPlayerIcons();
+   printf("Loading player colors from 'data/colors.txt'...\n");
    loadPlayerColors();
+   printf("Loading chapters from 'data/chapters.txt'...\n");
    loadChapters();
+   printf("Loading tiles from 'data/tiles.txt'...\n");
+   loadTiles();
+   printf("Loading levels from 'data/levels.txt'...\n");
    loadLevels();
+   printf("Loading successful!\n");
 }
 
 void loadPlayerIcons() {
@@ -34,7 +46,7 @@ void loadPlayerColors() {
       stream >> color.x >> color.y >> color.z;
 
       if (stream.rdbuf()->in_avail() != 0) {
-         printf("WARNING: Malformed line in 'data/colors.txt': '%s'. %ld excess characters, expected 3 real numbers.\n", line.c_str(), stream.rdbuf()->in_avail());
+         printf("WARNING: Malformed line: '%s'. %ld excess characters, expected 3 real numbers.\n", line.c_str(), stream.rdbuf()->in_avail());
          continue;
       }
       playerColorContainer.push_back(color);
@@ -45,82 +57,166 @@ void loadChapters() {
    chapterContainer = getLinesFromFileIgnoringCommentsAndEmptyLines("data/chapters.txt");
 }
 
+template<typename T>
+void getFieldAsSimpleValue(std::stringstream &stream, const std::string &value, const std::string &line, T &target) {
+   stream.clear();
+   stream.str(value);
+   stream >> target;
+
+   if (stream.rdbuf()->in_avail() != 0) {
+      printf("WARNING: Malformed line '%s'. Expected number.\n", line.c_str());
+   }
+}
+
 void loadLevels() {
    std::vector<std::string> lines = getLinesFromFileIgnoringCommentsAndEmptyLines("data/levels.txt");
    std::stringstream stream;
    levelContainer.reserve(lines.size() / 8); // rough estimate
-   Level stack;
+   Level stack, defaultLevel;
+   bool defaulting = false;
 
    for (const std::string &line: lines) {
+      if (line == "[DEFAULT]") {
+         defaulting = true;
+         continue;
+      }
+      
       if (line == "[END]") {
-         levelContainer.push_back(stack);
-         stack = {};
+         if (defaulting) {
+            defaulting = false;
+            stack = defaultLevel;
+         }
+         else {
+            levelContainer.push_back(stack);
+            stack = defaultLevel;
+         }
          continue;
       }
 
       size_t equals = line.find('=');
       if (equals == std::string::npos) {
-         printf("WARNING: Malformed line in 'data/levels.txt': '%s'. Expected '=' character.\n", line.c_str());
+         printf("WARNING: Malformed line: '%s'. Expected '=' character.\n", line.c_str());
          continue;
       }
 
+      Level level = defaulting ? defaultLevel : stack;
       std::string field = line.substr(0, equals);
       std::string value = line.substr(equals + 1);
       field.erase(field.find_last_not_of(" \n\r\t\v\f") + 1);
       value.erase(0, value.find_first_not_of(" \n\r\t\v\f"));
 
       if (field == "name") {
-         stack.name = value;
+         level.name = value;
       }
       else if (field == "chapter") {
-         stack.chapter = value;
+         level.chapter = value;
       }
       else if (field == "coinTile") {
-         // TODO: fix when add tile loading
-         // stack.coinTile = value;
-      }
-      else if (field == "sizeX") {
-         stream.clear();
-         stream.str(value);
-         stream >> stack.sizeX;
-
-         if (stream.rdbuf()->in_avail() != 0) {
-            printf("WARNING: Malformed line in 'data/levels.txt': '%s'. Expected number.\n", line.c_str());
+         if (!tileNameMap.count(value)) {
+            printf("WARNING: Malformed line: '%s'. No such tile '%s'\n", line.c_str(), value.c_str());
+         }
+         else {
+            level.coinTile = tileNameMap[value];
          }
       }
-      else if (field == "sizeY") {
-         stream.clear();
-         stream.str(value);
-         stream >> stack.sizeY;
-
-         if (stream.rdbuf()->in_avail() != 0) {
-            printf("WARNING: Malformed line in 'data/levels.txt': '%s'. Expected number.\n", line.c_str());
-         }
+      else if (field == "width") {
+         getFieldAsSimpleValue(stream, value, line, level.sizeX);
+      }
+      else if (field == "height") {
+         getFieldAsSimpleValue(stream, value, line, level.sizeY);
       }
       else if (field == "floor" || field == "map") {
-         std::vector<std::vector<size_t>> &container = (field == "map" ? stack.map : stack.floor);
-         container.reserve(stack.sizeY);
+         std::vector<std::vector<size_t>> &container = (field == "map" ? level.map : level.floor);
+         container.reserve(level.sizeY);
          stream.clear();
          stream.str(value);
 
-         for (size_t y = 0; y < stack.sizeY; ++y) {
+         for (size_t y = 0; y < level.sizeY; ++y) {
             std::vector<size_t> row;
-            row.reserve(stack.sizeX);
+            row.reserve(level.sizeX);
 
-            for (size_t x = 0; x < stack.sizeX; ++x) {
+            for (size_t x = 0; x < level.sizeX; ++x) {
                size_t value = 0;
                stream >> value;
+         
                row.push_back(value);
             }
             container.push_back(row);
          }
 
          if (stream.rdbuf()->in_avail() != 0) {
-            printf("WARNING: Malformed line in 'data/levels.txt': '%s'. Expected %lu number array. Fields 'sizeX' and 'sizeY' must be defined before fields 'floor' and 'map'.\n", line.c_str(), stack.sizeX * stack.sizeY);
+            printf("WARNING: Malformed line: '%s'. Expected %lu number array. Fields 'sizeX' and 'sizeY' must be defined before fields 'floor' and 'map'.\n", line.c_str(), level.sizeX * level.sizeY);
          }
       }
       else {
-         printf("WARNING: Malformed line in 'data/levels.txt': '%s'. Unexpected field '%s'.\n", line.c_str(), field.c_str());
+         printf("WARNING: Malformed line: '%s'. Unexpected field '%s'.\n", line.c_str(), field.c_str());
+      }
+   }
+}
+
+void loadTiles() {
+   std::vector<std::string> lines = getLinesFromFileIgnoringCommentsAndEmptyLines("data/tiles.txt");
+   std::stringstream stream;
+   tileContainer.reserve(lines.size() / 4); // rough estimate
+   Tile stack, defaultTile;
+   bool defaulting = false;
+
+   for (const std::string &line: lines) {
+      if (line == "[DEFAULT]") {
+         defaulting = true;
+         continue;
+      }
+      
+      if (line == "[END]") {
+         if (defaulting) {
+            defaulting = false;
+            stack = defaultTile;
+         }
+         else {
+            tileContainer.push_back(stack);
+            stack = defaultTile;
+         }
+         continue;
+      }
+
+      size_t equals = line.find('=');
+      if (equals == std::string::npos) {
+         printf("WARNING: Malformed line: '%s'. Expected '=' character.\n", line.c_str());
+         continue;
+      }
+
+      Tile tile = defaulting ? defaultTile : stack;
+      std::string field = line.substr(0, equals);
+      std::string value = line.substr(equals + 1);
+      field.erase(field.find_last_not_of(" \n\r\t\v\f") + 1);
+      value.erase(0, value.find_first_not_of(" \n\r\t\v\f"));
+
+      if (field == "tsize") {
+         getFieldAsSimpleValue(stream, value, line, tile.tsize);
+      }
+      else if (field == "width") {
+         getFieldAsSimpleValue(stream, value, line, tile.width);
+      }
+      else if (field == "height") {
+         getFieldAsSimpleValue(stream, value, line, tile.height);
+      }
+      else if (field == "type") {
+         if (value == "solid") {
+            tile.type = Tile::Type::solid;
+         }
+         else if (value == "coin") {
+            tile.type = Tile::Type::coin;
+         }
+         else {
+            printf("WARNING: Malformed line: '%s'. Invalid type '%s'.\n", line.c_str(), value.c_str());
+         }
+      }
+      else if (field == "texture") {
+         tile.texture = &getTexture(value);
+         tileNameMap[value] = tileContainer.size();
+      }
+      else {
+         printf("WARNING: Malformed line: '%s'. Unexpected field '%s'.\n", line.c_str(), field.c_str());
       }
    }
 }
@@ -187,6 +283,22 @@ Level &getLevel(size_t ID) {
 
 std::vector<Level> &getLevelContainer() {
    return levelContainer;
+}
+
+size_t getTileCount() {
+   return tileContainer.size();
+}
+
+Tile &getTile(size_t ID) {
+   if (ID >= getTileCount()) {
+      printf("ERROR: Tile ID out of bounds. Size is %lu while ID - %lu.\n", getTileCount(), ID);
+      exit(EXIT_FAILURE);
+   }
+   return tileContainer[ID];
+}
+
+std::vector<Tile> &getTileContainer() {
+   return tileContainer;
 }
 
 // player data module
